783a51dae46fbebdac8ca1b61a5d7ee866f4cd12
jettieb <bokjihee@naver.com>
2026-02-01 00:38:08 +0900

    chore: 초기 환경세팅

---
+++ src/test/java/ctrlS/totori/TotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package ctrlS.totori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
