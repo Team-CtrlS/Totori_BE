425dee7551310de5f96f101c70a185811b366aad
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-22 02:36:46 +0900

    [feat] 에러코드 추가 및 수정

--- src/main/java/ctrlS/totori/global/exception/ErrorCode.java
+++ src/main/java/ctrlS/totori/global/exception/ErrorCode.java
@@ -10,8 +10,12 @@ public enum ErrorCode {
     ERROR(400, "요청 처리에 실패했습니다."),
     UNAUTHORIZED_ACCESS(401, "인증되지 않은 사용자입니다."),
 
-    // auth
-    USER_NOT_FOUND(401, "존재하는 사용자가 없습니다.");
+    // Member
+    USER_NOT_FOUND(404, "존재하는 사용자가 없습니다."),
+    STAT_NOT_FOUND(404, "사용자의 통계 정보를 찾을 수 없습니다."),
+
+    // Badge
+    BADGE_NOT_FOUND(404, "보유한 뱃지가 없습니다.");
 
     private final int status;
     private final String message;
