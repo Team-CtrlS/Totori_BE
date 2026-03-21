318e52a76877c8b19505c996b7b7c0d5780c744e
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-22 02:39:13 +0900

    feat: 뱃지 controller 구현

---
+++ src/main/java/ctrlS/totori/badge/controller/BadgeController.java
@@ -0,0 +1,54 @@
+package ctrlS.totori.badge.controller;
+
+import ctrlS.totori.auth.service.CustomOAuth2UserService;
+import ctrlS.totori.badge.dto.BadgeResponseDto;
+import ctrlS.totori.badge.dto.CategoryBadgeResponseDto;
+import ctrlS.totori.badge.dto.MemberBadgeResponseDto;
+import ctrlS.totori.badge.entity.BadgeCategory;
+import ctrlS.totori.badge.entity.MemberBadge;
+import ctrlS.totori.badge.service.BadgeService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/badges")
+@RequiredArgsConstructor
+public class BadgeController {
+
+    private final BadgeService badgeService;
+    private final ctrlS.totori.member.repository.MemberStatRepository memberStatRepository;
+    private final ctrlS.totori.member.repository.MemberRepository memberRepository; // Member 찾기용
+
+    // 전체 뱃지 조회
+    @GetMapping
+    public ResponseEntity<List<BadgeResponseDto>> getAllBadges() {
+        return ResponseEntity.ok(badgeService.getAllBadges());
+    }
+
+    // 유저의 뱃지 조회
+    @GetMapping("/my")
+    public ResponseEntity<List<MemberBadgeResponseDto>> getMemberBadges(@AuthenticationPrincipal Long memberId) {
+        return ResponseEntity.ok(badgeService.getMemberBadges(memberId));
+    }
+
+    // 대표 뱃지 조회
+    @GetMapping("/my/representative")
+    public ResponseEntity<MemberBadgeResponseDto> getRepresentativeBadge(@AuthenticationPrincipal Long memberId) {
+        return ResponseEntity.ok(badgeService.getRepresentativeBadge(memberId));
+    }
+
+    // 특정 카테고리의 상세 뱃지 정보 조회
+    @GetMapping("/my/categories/{category}")
+    public ResponseEntity<CategoryBadgeResponseDto> getCategoryBadgeDetails(
+            @AuthenticationPrincipal Long memberId,
+            @PathVariable BadgeCategory category) {
+        return ResponseEntity.ok(badgeService.getCategoryBadgeDetail(memberId, category));
+    }
+}
