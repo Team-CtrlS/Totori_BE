f8ef56eeca11c3b8ad174b97ccad3e3bfc393bd6
nomellc <127232686+nomellc@users.noreply.github.com>
2026-03-26 01:17:00 +0900

    chore: CustomAuthenticationException 사용되지 않는 코드 제거

--- src/main/java/ctrlS/totori/global/security/CustomAuthenticationException.java
+++ src/main/java/ctrlS/totori/global/security/CustomAuthenticationException.java
@@ -3,14 +3,8 @@ package ctrlS.totori.global.security;
 import ctrlS.totori.global.exception.ErrorCode;
 
 public class CustomAuthenticationException extends RuntimeException {
-    private final ErrorCode errorCode;
-
     public CustomAuthenticationException(ErrorCode errorCode) {
         super(errorCode.getMessage());
-        this.errorCode = errorCode;
     }
 
-    public ErrorCode getErrorCode() {
-        return errorCode;
-    }
 }
