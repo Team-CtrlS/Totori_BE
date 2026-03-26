a3ed57ca50cca739baeb87dc8e724fac7b875f58
nomellc <127232686+nomellc@users.noreply.github.com>
2026-03-26 23:47:23 +0900

    refactor:ErrorCode 메시지 추가

--- src/main/java/ctrlS/totori/global/exception/ErrorCode.java
+++ src/main/java/ctrlS/totori/global/exception/ErrorCode.java
@@ -23,6 +23,8 @@ public enum ErrorCode {
     LOGGED_OUT_TOKEN(401, "이미 로그아웃된 토큰입니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(401, "만료된 토큰입니다."),
+    UNAUTHORIZED(401, "인증이 필요합니다."),
+    ACCESS_DENIED(403, "접근 권한이 없습니다."),
 
     // connect
     ONLY_CHILD_CAN_CREATE_CONNECT_CODE(403, "아동 계정만 연결 코드를 생성할 수 있습니다."),
