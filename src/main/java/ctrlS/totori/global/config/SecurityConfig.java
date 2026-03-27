0a7408c4f2645f9a8f4e519dd93374d20a48f8d2
nomellc <127232686+nomellc@users.noreply.github.com>
2026-03-27 20:09:52 +0900

    fix: OAuth2 인증 에러 리다이렉트 오류 해결

--- src/main/java/ctrlS/totori/global/config/SecurityConfig.java
+++ src/main/java/ctrlS/totori/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ctrlS.totori.global.config;
 
 import ctrlS.totori.auth.service.CustomOAuth2UserService;
+import ctrlS.totori.global.exception.ErrorCode;
 import ctrlS.totori.global.security.JwtAuthenticationFilter;
 import ctrlS.totori.global.security.oauth.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
@@ -33,8 +34,33 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(ErrorCode.UNAUTHORIZED_ACCESS.getStatus());
+                            response.setContentType("application/json;charset=UTF-8");
+                            response.getWriter().write(
+                                    String.format(
+                                            "{\"status\":%d,\"message\":\"%s\"}",
+                                            ErrorCode.UNAUTHORIZED_ACCESS.getStatus(),
+                                            ErrorCode.UNAUTHORIZED_ACCESS.getMessage()
+                                    )
+                            );
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(ErrorCode.ACCESS_DENIED.getStatus());
+                            response.setContentType("application/json;charset=UTF-8");
+                            response.getWriter().write(
+                                    String.format(
+                                            "{\"status\":%d,\"message\":\"%s\"}",
+                                            ErrorCode.ACCESS_DENIED.getStatus(),
+                                            ErrorCode.ACCESS_DENIED.getMessage()
+                                    )
+                            );
+                        })
+                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/login/success").permitAll()
