60f8987784abd57397471535a61d370382eed953
nomellc <127232686+nomellc@users.noreply.github.com>
2026-04-18 17:04:23 +0900

    [refactor] AttendanceResponse 모호한 attendedToday 필드명 변경
    // 오늘 처음 출석하면 true, 아니면 false
    attendedToday -> newlyAttended

--- src/main/java/ctrlS/totori/attendance/dto/AttendanceResponse.java
+++ src/main/java/ctrlS/totori/attendance/dto/AttendanceResponse.java
@@ -1,10 +1,10 @@
 package ctrlS.totori.attendance.dto;
 
 public record AttendanceResponse(
-        boolean attendedToday,
+        boolean newlyAttended, // 오늘 처음 출석하면 true, 아니면 false
         int totalAttendanceDays
 ) {
-    public static AttendanceResponse of(boolean attendedToday, int totalAttendanceDays) {
-        return new AttendanceResponse(attendedToday, totalAttendanceDays);
+    public static AttendanceResponse of(boolean newlyAttended, int totalAttendanceDays) {
+        return new AttendanceResponse(newlyAttended, totalAttendanceDays);
     }
 }
