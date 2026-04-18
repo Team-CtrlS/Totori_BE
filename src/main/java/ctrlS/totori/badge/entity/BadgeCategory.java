241cff9d18961be2c2ee724a8a29979ae4f9dfef
nomellc <127232686+nomellc@users.noreply.github.com>
2026-04-18 16:42:14 +0900

    [refactor] BadgeCategory 출석 뱃지 획득 필드 변경

--- src/main/java/ctrlS/totori/badge/entity/BadgeCategory.java
+++ src/main/java/ctrlS/totori/badge/entity/BadgeCategory.java
@@ -1,6 +1,5 @@
 package ctrlS.totori.badge.entity;
 
-import ctrlS.totori.member.entity.Member;
 import ctrlS.totori.member.entity.MemberStat;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +11,7 @@ import java.util.function.Function;
 public enum BadgeCategory {
     BOOK_CREATED("상상력의 마법사", MemberStat::getTotalCreatedBooks),
     BOOK_READ("지혜의 수집가", MemberStat::getTotalReadBooks),
-    ATTENDANCE("숲속의 단골손님", MemberStat::getConsecutiveAttendanceDays),
+    ATTENDANCE("숲속의 단골손님", MemberStat::getTotalAttendanceDays),
     ACORN("도토리 부자", MemberStat::getTotalAcquiredAcorn);
 
     private final String title;
