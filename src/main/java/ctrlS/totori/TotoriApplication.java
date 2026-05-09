164f18c2603db07bf9777fb3367141d8879a6023
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-05-10 03:23:21 +0900

    [feat] 비동기 어노테이션 추가

--- src/main/java/ctrlS/totori/TotoriApplication.java
+++ src/main/java/ctrlS/totori/TotoriApplication.java
@@ -3,7 +3,9 @@ package ctrlS.totori;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @EnableJpaAuditing
 public class TotoriApplication {
